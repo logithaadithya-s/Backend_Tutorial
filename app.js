51d6918399f5b9997604e93ae6ed8d83784f42f0
LOGITH AADITHYA S <logithjee@gmail.com>
2026-01-02 11:55:34 +0530

    middleware

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 import express from 'express';
-
-const app = express();
-
+import cookieParser from 'cookie-parser';
 import userRouter from './Routes/user.routes.js';
 import subRouter from './Routes/subscription.routes.js';
 import authRouter from './Routes/auth.routes.js';
-
+import errorMiddleware from './middleware/error.middleware.js';
 import connectToDatabase from './Database/mongobd.js';
+
+const app = express();
+
 const PORT = process.env.PORT;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+app.use(cookieParser())
+app.use(errorMiddleware);
+
+
 app.use("/api/v1/auth",authRouter);
-
 app.use("/api/v1/user",userRouter);
-
 app.use("/api/v1/subscriptions",subRouter);
 
 app.get('/',(req,res)=>{
